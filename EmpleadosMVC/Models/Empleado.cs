7dc086933564367da6f339dd1158f555c6d5a42c
Ruben Berreguero <rubenbr85@gmail.com>
2019-03-25 17:05:15 +0100

    Crear modelo Empleado

---
+++ EmpleadosMVC/Models/Empleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EmpleadosMVC.Models
+{
+    public class Empleado
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int antiguedad { get; set;}
+        public int edad { get; set; }
+    }
+
+    public class EmpleadoDBContext : DbContext
+    {
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
