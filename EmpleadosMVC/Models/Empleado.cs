b04a3f9a429ed1c061c24a4be3e0f6534cc71eed
Ruben Berreguero <rubenbr85@gmail.com>
2019-03-31 13:13:42 +0200

    Modelo Emplado - anotaciones para campos requeridos. Realizar migracion

--- EmpleadosMVC/Models/Empleado.cs
+++ EmpleadosMVC/Models/Empleado.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace EmpleadosMVC.Models
 {
     public class Empleado
     {
         public int id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength =4)]
         public string nombre { get; set; }
+
+        [Required]
+        [Range(1,40)]
         public int antiguedad { get; set;}
+
+        [Required]
+        [Range (18,65)]
         public int edad { get; set; }
+
+        [Required]
         public string categoria { get; set; }
     }
 
